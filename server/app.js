ca0ea5cd0e21d7a08899a739f04900254c9da4f5
Lilith-zny <lilith.zny@gmail.com>
2024-10-05 16:43:34 +0700

    server

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Hello World'
+    })
+})
+
+
+
+app.listen(5000, () => console.log(`Server is running at http://localhost:5000`))
